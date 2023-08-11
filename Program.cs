d1b037654a65e5fc5da8fab44251677ee45800c9
Victor <victorrefundine@hotmail.com>
2023-08-10 22:34:59 -0300

    Inclusão Haikai de Paulo Leminski.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Inverno");
+Console.WriteLine("É tudo o que sinto");
+Console.WriteLine("Viver");
+Console.WriteLine("É sucinto");
+Console.WriteLine("");
+Console.WriteLine("Paulo Leminski");
